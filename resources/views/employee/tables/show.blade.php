7a4e9c191ddf52c11cd6dcf65ba4af3f8d1d4c6e
AbrarZShahriar <a.zshahriar@gmail.com>
2018-08-28 17:09:17 +0600

    Your Message

--- resources/views/employee/tables/show.blade.php
+++ resources/views/employee/tables/show.blade.php
@@ -1,15 +1,19 @@
 @extends('employee/layouts/employeeLayout')
 
-@section('title', 'Employee Tables')
+@section('title', 'Employee Categories')
 
 @section('main_content')
 
     <div class="container">
         <div class="row">
-            <div class="col-md-12">
+            <div class="col-md-6">
                 <p class="display-4">
                     Listing All Tables..
                 </p>
+            </div>
+            <div class="col-md-6 new-item">
+                {{-- <a href="{{route('admin.showAvailableTables')}}" type="button" class="btn btn-lg btn-success" style="float:right; margin-left:1rem;"><i class="fas fa-check"></i> Show Available Tables</a>
+                <a href="{{route('admin.showOccupiedTables')}}" type="button" class="btn btn-lg btn-danger" style="float:right;"><i class="fas fa-exclamation-triangle"></i> Show Occupied Tables</a> --}}
             </div>
         </div>
         <hr>
@@ -20,55 +24,30 @@
                         <tr class="table-header table-row">
                           <th scope="col">Table ID</th>
                           <th scope="col">Status</th>
+                          <th scope="col">Capacity</th>
                           <th scope="col">Action</th>
                         </tr>
                       </thead>
                       <tbody>
-                        <tr class="table-row">
-                          <th scope="row">1</th>
-                          <td><span class="badge badge-pill badge-success">Available</span></td>
-                          <td>
-                              <button type="button" class="btn">
-                                  Toggle Status
-                              </button>
-                          </td>
-                        </tr>
-                        <tr class="table-row">
-                          <th scope="row">2</th>
-                          <td><span class="badge badge-pill badge-danger">Occupied</span></td>
-                          <td>
-                              <button type="button" class="btn">
-                                  Toggle Status
-                              </button>
-                          </td>
-                        </tr>
-                        <tr class="table-row">
-                          <th scope="row">3</th>
-                          <td>300</td>
-                          <td>
-                              <button type="button" class="btn">
-                                  Toggle Status
-                              </button>
-                          </td>
-                        </tr>
-                        <tr class="table-row">
-                          <th scope="row">4</th>
-                          <td>300</td>
-                          <td>
-                              <button type="button" class="btn">
-                                  Toggle Status
-                              </button>
-                          </td>
-                        </tr>
-                        <tr class="table-row">
-                          <th scope="row">5</th>
-                          <td>300</td>
-                          <td>
-                              <button type="button" class="btn">
-                                  Toggle Status
-                              </button>
-                          </td>
-                        </tr>
+                          @foreach($tables as $table)
+                            <tr class="table-row">
+                              <th scope="row">{{$table->id}}</th>
+                              <td><span class="badge badge-pill {{$table->status == 'Available' ? 'badge-success' : 'badge-danger'}}">{{$table->status}}</span></td>
+                              <td scope="row">{{$table->capacity}}</td>
+                              <td>
+                                  <form method="post">
+                                      {{csrf_field()}}
+                                      <input type="hidden" name="table_id" value="{{$table->id}}">
+                                      <button type="submit" class="btn btn-light">
+                                          Toggle Status
+                                      </button>
+                                      <a href="{{route('admin.removeTable', ['id' => $table->id])}}" type="submit" class="btn btn-danger">
+                                          Remove Table
+                                      </a>
+                                  </form>
+                              </td>
+                            </tr>
+                            @endforeach
                       </tbody>
                 </table>
             </div>
